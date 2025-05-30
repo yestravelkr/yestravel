8aa4589d4c49bf8af69b2df8eb7fca2b4442158e
Jun <qurugi0347@gmail.com>
2025-05-31 00:32:14 +0900

    CHORE: remove swagger setting

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { setupSwagger } from '@src/common/swagger-setup';
 import { AppModule } from './app.module';
 import { DataSources } from '@src/database/datasources';
 
 async function bootstrap() {
   await DataSources.yestravel.initialize();
   const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
