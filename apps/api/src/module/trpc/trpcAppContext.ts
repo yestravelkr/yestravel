eee994de34189ed8b22d1b08cbc23de2ad908396
Jun <qurugi0347@gmail.com>
2025-06-11 00:51:27 +0900

    FEAT: TRPC에서 Req, Res 관리를 위한 Context 처리

---
+++ apps/api/src/module/trpc/trpcAppContext.ts
@@ -0,0 +1,10 @@
+import {Injectable} from "@nestjs/common";
+import {ContextOptions} from "nestjs-trpc";
+
+
+@Injectable()
+export class TRPCAppContext {
+  async create(opts: ContextOptions) {
+    return opts
+  }
+}
