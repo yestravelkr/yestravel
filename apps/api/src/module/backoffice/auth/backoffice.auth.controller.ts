030f19114fe32c7fdb8ebb467446ee543236b5e4
Jun <qurugi0347@gmail.com>
2025-06-11 01:01:03 +0900

    FEAT: 로그인 처리

--- apps/api/src/module/backoffice/auth/backoffice.auth.controller.ts
+++ apps/api/src/module/backoffice/auth/backoffice.auth.controller.ts
@@ -3,6 +3,7 @@ import {BackofficeAuthService} from "@src/module/backoffice/auth/backoffice.auth
 import {Controller, Inject} from "@nestjs/common";
 import {Transactional} from "@src/module/shared/transaction/transaction.decorator";
 import {TransactionService} from "@src/module/shared/transaction/transaction.service";
+import {Ctx} from "nestjs-trpc";
 
 @Controller()
 export class BackofficeAuthController {
@@ -22,6 +23,12 @@ export class BackofficeAuthController {
 
     // 현재는 단순히 성공 메시지를 반환합니다.
     return { message: '회원가입이 완료되었습니다.' };
+  }
+
+  @MessagePattern('backoffice.auth.login')
+  async login(data: {email: string, password: string}): Promise<{ accessToken: string, refreshToken: string }> {
+    const { email, password } = data;
+    return this.backofficeAuthService.login(email, password);
   }
 
 }
