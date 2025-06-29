b73d500c90be162bda2beb0668a74bbe4ff164bc
Jun <qurugi0347@gmail.com>
2025-06-29 16:27:00 +0900

    FEAT: trpc 공유용 packages 세팅

---
+++ packages/api-types/src/server.ts
@@ -0,0 +1,33 @@
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
+
+const t = initTRPC.create();
+const publicProcedure = t.procedure;
+
+const appRouter = t.router({
+  backofficeAuth: t.router({
+    register: publicProcedure.input(z.object({
+      email: z.string().email('이메일 형식이 아닙니다.'),
+      password: z.string().min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
+    })).output(z.object({
+      message: z.string(),
+    })).mutation(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+    login: publicProcedure.input(z.object({
+      email: z.string().email('이메일 형식이 아닙니다.'),
+      password: z.string(),
+    })).output(z.object({
+      accessToken: z.string(),
+    })).mutation(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+    test: publicProcedure.output(z.object({
+      message: z.string(),
+    })).query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any)
+  }),
+  sample: t.router({
+    getHello: publicProcedure.input(z.object({
+      name: z.string().optional(),
+    })).output(z.string()).query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+    getSample: publicProcedure.output(z.string()).query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any)
+  })
+});
+export type AppRouter = typeof appRouter;
+
