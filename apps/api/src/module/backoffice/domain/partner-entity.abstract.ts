e66201c5af1b82db445de5bdc5f06d59ace4190e
chotaegyu <chotg5592@gmail.com>
2025-07-08 23:26:34 +0900

    FEAT: 파트너 엔티티 수정

--- apps/api/src/module/backoffice/domain/partner-entity.abstract.ts
+++ apps/api/src/module/backoffice/domain/partner-entity.abstract.ts
@@ -1,6 +1,7 @@
 import { Column, DeleteDateColumn } from 'typeorm';
 import { BaseEntity } from '@src/module/backoffice/domain/base.entity';
-import { BusinessType } from '@src/module/backoffice/domain/social-media-platform.enum';
+import { BusinessInfoEntity } from '@src/module/backoffice/domain/business-info.entity';
+import { BankEntity } from '@src/module/backoffice/domain/bank.entity';
 
 export abstract class PartnerEntity extends BaseEntity {
   @Column({ length: 100, type: 'varchar' })
@@ -21,74 +22,12 @@ export abstract class PartnerEntity extends BaseEntity {
   })
   thumbnail?: string;
 
-  // 사업자 정보
-  @Column({
-    name: 'business_type',
-    type: 'enum',
-    enum: BusinessType,
-    nullable: true,
-  })
-  businessType?: BusinessType;
+  @Column(() => BusinessInfoEntity)
+  businessInfo?: BusinessInfoEntity;
 
-  @Column({
-    name: 'business_registration_number',
-    type: 'varchar',
-    length: 20,
-    nullable: true,
-  })
-  businessRegistrationNumber?: string;
-
-  @Column({
-    name: 'representative_name',
-    type: 'varchar',
-    length: 50,
-    nullable: true,
-  })
-  representativeName?: string;
-
-  // 은행 정보
-  @Column({ name: 'bank_name', type: 'varchar', length: 50, nullable: true })
-  bankName?: string;
-
-  @Column({
-    name: 'account_number',
-    type: 'varchar',
-    length: 50,
-    nullable: true,
-  })
-  accountNumber?: string;
-
-  @Column({
-    name: 'account_holder',
-    type: 'varchar',
-    length: 50,
-    nullable: true,
-  })
-  accountHolder?: string;
+  @Column(() => BankEntity)
+  bankInfo?: BankEntity;
 
   @DeleteDateColumn({ name: 'deleted_at', nullable: true })
   deletedAt?: Date;
-
-  getBankInfo() {
-    if (!this.bankName || !this.accountNumber || !this.accountHolder) {
-      return null;
-    }
-    return {
-      bankName: this.bankName,
-      accountNumber: this.accountNumber,
-      accountHolder: this.accountHolder,
-    };
-  }
-
-  getBusinessInfo() {
-    if (!this.businessType) {
-      return null;
-    }
-    return {
-      businessType: this.businessType,
-      businessRegistrationNumber: this.businessRegistrationNumber,
-      representativeName: this.representativeName,
-      email: this.email,
-    };
-  }
 }
