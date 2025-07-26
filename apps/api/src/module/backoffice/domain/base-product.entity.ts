a97e84fb981b409671e13a93e0060d946a599100
chotaegyu <chotg5592@gmail.com>
2025-07-26 20:55:07 +0900

    FEAT: campaign id 추가

--- apps/api/src/module/backoffice/domain/base-product.entity.ts
+++ apps/api/src/module/backoffice/domain/base-product.entity.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, TableInheritance } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  TableInheritance,
+} from 'typeorm';
 import { BaseEntity } from '@src/module/backoffice/domain/base.entity';
+import { CampaignEntity } from '@src/module/backoffice/domain/campaign.entity';
 
 export enum ProductType {
   HOTEL = 'HOTEL',
@@ -20,4 +27,11 @@ export abstract class BaseProductEntity extends BaseEntity {
 
   @Column()
   price: number;
+
+  @Column({ name: 'campaign_id', type: 'integer' })
+  campaignId: number;
+
+  @OneToOne(() => CampaignEntity, campaign => campaign.product)
+  @JoinColumn({ name: 'campaign_id' })
+  campaign: CampaignEntity;
 }
